a53d5d60f1407becdb439669e4eea23515245114
belimsameerkhan1 <belimsameerkhan527@gmail.com>
2026-07-26 13:07:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -165,12 +165,16 @@ let precedenceAxioms = [
   }
 ];
 
-function getNowFormatted() {
-  const now = new Date();
-  const datePart = now.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
-  const timePart = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
-  return `${datePart} ${timePart}`;
-}
+generatedOn: new Date().toLocaleString("en-IN", {
+  timeZone: "Asia/Kolkata",
+  day: "2-digit",
+  month: "short",
+  year: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hour12: true,
+})
 
 let verificationReports = [
   {
